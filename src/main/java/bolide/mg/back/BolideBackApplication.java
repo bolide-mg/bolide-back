2e36e64a0b9340f4016be65f2815caa8b226ddea
MiiJack <f.rama723@gmail.com>
2024-05-21 11:00:22 +0300

    Initialized Spring

---
+++ src/main/java/bolide/mg/back/BolideBackApplication.java
@@ -0,0 +1,13 @@
+package bolide.mg.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolideBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolideBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bolide/mg/back/BolideBackApplicationTests.java
@@ -0,0 +1,13 @@
+package bolide.mg.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolideBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
